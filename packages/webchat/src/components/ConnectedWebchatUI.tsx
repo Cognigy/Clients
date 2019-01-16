910faaee41dd6c0961cabfbd0d7f2e16f5227d8c
Robin Schuster <r.schuster@cognigy.com>
2019-01-16 15:47:02 +0100

    merge webchat packages, improve build pipeline

--- packages/webchat/src/components/ConnectedWebchatUI.tsx
+++ packages/webchat/src/components/ConnectedWebchatUI.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { HTMLProps } from 'react';
-import { WebchatUI, WebchatUIProps } from "@cognigy/webchat-ui";
+import { WebchatUI, WebchatUIProps } from "./WebchatUI";
 import { connect } from "react-redux";
 import { StoreState } from "../store/store";
 import { sendMessage } from '../store/messages/message-middleware';
--- packages/webchat/webpack.config.js
+++ packages/webchat/webpack.config.js
@@ -1,38 +1,3 @@
-// const path = require('path');
-// module.exports = {
-//     mode: 'production',
-
-//     entry: './src/webchat.tsx',
-//     output: {
-//         path: path.resolve(__dirname, 'lib'),
-//         filename: 'webchat.js'
-//     },
-
-//     resolve: {
-//         // Add `.ts` and `.tsx` as a resolvable extension.
-//         extensions: [".ts", ".tsx", ".js"]
-//     },
-
-//     module: {
-//         rules: [
-//             {
-//                 test: /\.tsx?$/,
-//                 use: [
-//                     {
-//                         loader: 'babel-loader',
-//                     }
-//                     {
-//                         loader: 'ts-loader',
-//                         options: {
-//                             transpileOnly: true
-//                         }
-//                     }
-//                 ]
-//             }
-//         ]
-//     }
-// }
-
 var path = require('path');
 
 module.exports = {
@@ -40,8 +5,8 @@ module.exports = {
     mode: 'production',
     entry: ['babel-polyfill', './src/webchat.tsx'],
     output: {
-        path: path.resolve(__dirname, 'lib'),
-        filename: 'webchat.js'
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'webchat.bundle.js'
     },
     resolve: {
         extensions: ['.ts', '.tsx', '.js', '.json']

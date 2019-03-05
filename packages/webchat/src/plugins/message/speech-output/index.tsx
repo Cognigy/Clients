88d09e6e4c30f3b48796d8e03e0ad755c3c76e53
Robin Schuster <r.schuster@cognigy.com>
2019-03-05 11:05:53 +0100

    optimized plugin size for speech output

--- packages/webchat/src/plugins/message/speech-output/index.tsx
+++ packages/webchat/src/plugins/message/speech-output/index.tsx
@@ -1,32 +1,38 @@
-import * as React from 'react';
-import { MessageComponentProps, MessagePlugin } from '../../../common/interfaces/message-plugin';
+import { MessageComponentProps, MessagePluginFactory, MessageMatcher } from '../../../common/interfaces/message-plugin';
 import { registerMessagePlugin } from '../../helper';
 
-const SpeechOutput = (props: MessageComponentProps) => {
-    if (!speechSynthesis)
+const speechOutput: MessagePluginFactory = ({ React }) => {
+    // only read out incoming messages with text
+    const match: MessageMatcher = ({ text, source }) => source === 'bot' && !!text;
+
+    const SpeechOutput = (props: MessageComponentProps) => {
+        // check whether text to speech is available in client
+        if (!speechSynthesis)
+            return null;
+
+        // check whether text to speech is enabled in endpoint
+        if (!props.config.settings.enableTTS)
+            return null;
+
+        // we already checked that text exists in the match function
+        const text = props.message.text as string;
+
+        const utterance = new SpeechSynthesisUtterance();
+        utterance.text = text;
+        speechSynthesis.speak(utterance);
+
         return null;
+    }
 
-    if (!props.config.settings.enableTTS)
-        return null;
+    // make sure to read out messages only once
+    const MemoizedSpeechOutput = React.memo(SpeechOutput);
 
-    const { text } = props.message;
-    if (!text)
-        return null;
-
-    const utterance = new SpeechSynthesisUtterance();
-    utterance.text = text;
-    speechSynthesis.speak(utterance);
-
-    return null;
-}
-
-const MemoizedSpeechOutput = React.memo(SpeechOutput);
-
-const speechOutput: MessagePlugin = {
-    match: ({ text, source }) => source === 'bot' && !!text,
-    component: MemoizedSpeechOutput,
-    options: {
-        passthrough: true
+    return {
+        match,
+        component: MemoizedSpeechOutput,
+        options: {
+            passthrough: true
+        }
     }
 }
 

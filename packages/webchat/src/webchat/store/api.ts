13377217de986c007a41450052e515e6eebb2aa8
Robin Schuster <r.schuster@cognigy.com>
2019-03-07 10:49:26 +0100

    add sendMessage to global api

--- packages/webchat/src/webchat/store/api.ts
+++ packages/webchat/src/webchat/store/api.ts
@@ -1,9 +1,11 @@
 import { createWebchatStore } from "./store";
 import { setOpen, toggleOpen } from "./ui/ui-reducer";
+import { sendMessage } from "./messages/message-middleware";
 
 
 export const createBrowserApi = ({ dispatch }: ReturnType<typeof createWebchatStore>) => ({
     open: () => { dispatch(setOpen(true)) },
     close: () => { dispatch(setOpen(false)) },
-    toggle: () => { dispatch(toggleOpen()) }
+    toggle: () => { dispatch(toggleOpen()) },
+    sendMessage: (text: string, data?: any) => { dispatch(sendMessage({ text, data }))}
 });
